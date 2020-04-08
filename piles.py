2b55ce1b7bc6ca081a9f9912e3549ce40d7bf503
Adoria298 <bananaorange29@outlook.com>
2020-04-08 15:28:38 +0100

    Deleted stuff no longer needed. Updated piles.py with proto3 stuff. Commented server.py

--- piles.py
+++ piles.py
@@ -2,7 +2,7 @@
 """
 Code for managing hands and decks of cards.
 """
-from cards import Card, NumCard, Draw2Card, ReverseCard, SkipCard, WildCard, WildDraw4Card, COLOURS
+from uno_pb2 import Card, CardColour
 import random
 from pprint import pprint
 from dataclasses import dataclass
@@ -43,60 +43,3 @@ class Pile:
         TODO: implement shuffling algorithm based on Spotify's playlist shuffle so that similar cards are far apart: https://codegolf.stackexchange.com/questions/198094/spotify-shuffle-music-playlist-shuffle-algorithm
         """
         random.shuffle(self.cards)  
-
-class DiscardPile(Pile):
-    """
-    Where cards are discarded to.
-    """
-    pass
-
-class DrawPile(Pile):
-    """
-    Where cards are drawn from.
-    """
-    pass
-
-class Deck(Pile):
-    """
-    A Deck contains 108 cards:
-     - 19 Blue
-     - 19 Green
-     - 19 Red
-     - 19 Yellow
-     - 8 Draw Two - 2 of each colour
-     - 8 Reverse - 2 of each colour
-     - 8 Skip - 2 of each colour
-     - 4 Wild
-     - 4 Wild Draw Four
-
-    The number cards have the numbers 0-9, with 1-9 repeated (i.e. they have two of every number
-    between 0 and 9 except 0, which they only have one of.)
-
-    This class creates all 108 cards.
-    """
-    def __init__(self):
-        super().__init__(cards=[])
-        for colour in COLOURS:
-            for i in range(0, 10): # stop point is exclusive
-                self.cards.append(NumCard(colour, value=i))
-            for i in range(1, 10): # start point is inclusive
-                self.cards.append(NumCard(colour, value=i))
-            for i in range(2):
-                self.cards.append(Draw2Card(colour))
-                self.cards.append(ReverseCard(colour))
-                self.cards.append(SkipCard(colour))
-        for i in range(4):
-            self.cards.append(WildCard())
-            self.cards.append(WildDraw4Card())
-        pprint(self.cards)
-
-
-
-class Hand(Pile):
-    """
-    A single player's hand.
-    """
-    pass
-
-if __name__ == "__main__":
-    d = Deck()
--- server.py
+++ server.py
@@ -1,14 +1,12 @@
 """
 Uses code based on protocol buffers file `uno.proto` to create a GRPC server.
 """
-import uno_pb2 as uno_pb2
-import uno_pb2_grpc as uno_pb2_grpc
-
+# proto3 generated code
+import uno_pb2
+import uno_pb2_grpc
+# homemade code
 from deck import Deck
 
-# manually check files compiled correctly
-#print(help(uno_pb2))
-#print(help(uno_pb2_grpc))
 
 if __name__ == "__main__":
     from pprint import pprint
