a8e3d2800a38b945fe711c714920572909bfe397
Adoria298 <bananaorange29@outlook.com>
2020-04-08 17:13:04 +0100

    Implemented fake UnoServicer.

--- server.py
+++ server.py
@@ -7,7 +7,54 @@ import uno_pb2_grpc
 # homemade code
 from deck import Deck
 
+class UnoServicer(uno_pb2_grpc.UnoServicer):
+    """
+    Runs the game.
+    """
 
+    def RequestStateOfPlay(self, request, context):
+        print(f"State of Play requested by {request.name}")
+        return uno_pb2.StateOfPlay(round_num = 3,
+            players = [uno_pb2.Player(
+                    hand = Deck().cards[:6],
+                    name="Test",
+                    uno_declared = False,
+                    score = 0),
+                uno_pb2.Player(
+                    hand = Deck().cards[7],
+                    name="Test 2",
+                    uno_declared = True,
+                    score = 0)],
+            current_player = 1,
+            discard_pile = Deck().cards[8:53],
+            draw_pile = Deck().cards[54:89],
+            round_over = False,
+            game_over = False)
+    
+    def PlayCard(self, request, context):
+        print(f"{request} played.")
+        return uno_pb2.StateOfPlay(round_num = 3,
+            players = [uno_pb2.Player(
+                    hand = Deck().cards[:6],
+                    name="Test",
+                    uno_declared = False,
+                    score = 0),
+                uno_pb2.Player(
+                    hand = Deck().cards[7],
+                    name="Test 2",
+                    uno_declared = True,
+                    score = 0)],
+            current_player = 1,
+            discard_pile = Deck().cards[8:53],
+            draw_pile = Deck().cards[54:89],
+            round_over = False,
+            game_over = False)
+
+    def DrawCard(self, request, context):
+        print(f"{request.name} wants to draw a card!")
+        return uno_pb2.Card(colour=3,# green
+            action=4, # skip
+            value=20)
 if __name__ == "__main__":
     from pprint import pprint
     pprint(Deck().cards)
