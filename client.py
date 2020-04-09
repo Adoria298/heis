1e786b526ec865dd9399328b66d094e498dd3985
Adoria298 <bananaorange29@outlook.com>
2020-04-09 16:25:54 +0100

    Made a client and server that understand each other.

--- client.py
+++ client.py
@@ -15,13 +15,12 @@ stub = uno_pb2_grpc.UnoStub(channel)
 
 print("Getting state.")
 state = stub.RequestStateOfPlay(uno_pb2.Player(hand=[
-    uno_pb2.Card(colour=3, action=4, value=20), # a green skip
-    uno_pb2.Card(colour=5, action=5, value=50), # an undecided wild
-    uno_pb2.Card(colour=1, action=1, value=1)], # a red 1
+        uno_pb2.Card(colour=3, action=4, value=20), # a green skip
+        uno_pb2.Card(colour=5, action=5, value=50), # an undecided wild
+        uno_pb2.Card(colour=1, action=1, value=1)], # a red 1
     name="Test Player",
     uno_declared=False,
-    score=72
-))
+    score=72))
 pprint(stub)
 
 print("Playing a green skip.")
@@ -30,10 +29,9 @@ pprint(new_state)
 
 print("Drawing card.")
 new_card = stub.DrawCard(uno_pb2.Player(hand=[
-    uno_pb2.Card(colour=5, action=5, value=50), # an undecided wild
+    uno_pb2.Card(colour=5, action=5, value=50), # an undecided (black) wild
     uno_pb2.Card(colour=1, action=1, value=1)], # a red 1
     name="Test Player",
     uno_declared=False,
-    score=72
-))
+    score=72))
 pprint(new_card)
--- server.py
+++ server.py
@@ -20,17 +20,18 @@ class UnoServicer(uno_pb2_grpc.UnoServicer):
 
     def RequestStateOfPlay(self, request, context):
         print(f"State of Play requested by {request.name}")
-        return uno_pb2.StateOfPlay(round_num = 3,
+        return uno_pb2.StateOfPlay(
+            round_num = 3,
             players = [uno_pb2.Player(
                     hand = Deck().cards[:6],
                     name="Test",
                     uno_declared = False,
-                    score = 0),
+                    score = 65),
                 uno_pb2.Player(
-                    hand = Deck().cards[7],
+                    hand = Deck().cards[7:9], # has to be a list of cards
                     name="Test 2",
-                    uno_declared = True,
-                    score = 0)],
+                    uno_declared = False,
+                    score = 72)],
             current_player = 1,
             discard_pile = Deck().cards[8:53],
             draw_pile = Deck().cards[54:89],
@@ -46,9 +47,9 @@ class UnoServicer(uno_pb2_grpc.UnoServicer):
                     uno_declared = False,
                     score = 0),
                 uno_pb2.Player(
-                    hand = Deck().cards[7],
+                    hand = Deck().cards[7:9],
                     name="Test 2",
-                    uno_declared = True,
+                    uno_declared = False,
                     score = 0)],
             current_player = 1,
             discard_pile = Deck().cards[8:53],
@@ -64,7 +65,7 @@ class UnoServicer(uno_pb2_grpc.UnoServicer):
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    uno_pb2_grpc.add_UnoServicer_to_server(UnoServicer, server)
+    uno_pb2_grpc.add_UnoServicer_to_server(UnoServicer(), server)
     server.add_insecure_port("[::]:50051")
     server.start()
     try:
@@ -74,3 +75,6 @@ def serve():
 
 if __name__ == "__main__":
     serve()
+    # test code
+    #s = UnoServicer()
+    #print(s.PlayCard(uno_pb2.Card(colour=3, action=5, value=50), "")) # a wild that changes the colour to green
