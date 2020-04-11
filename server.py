5b0d001a59ace5ab34d64e4de98f26c4fdecb873
Adoria298 <bananaorange29@outlook.com>
2020-04-11 10:12:40 +0100

    Amended server.py to do things more properly.

--- server.py
+++ server.py
@@ -17,29 +17,27 @@ from deck import Deck
 class UnoServicer(uno_pb2_grpc.UnoServicer):
     def __init__(self):
         super().__init__() # just in case
-        self.deck = Deck().cards
-        random.shuffle(self.deck)
+        self.draw_pile = Deck().cards
+        random.shuffle(self.draw_pile)
         self.players = []
+        self.discard_pile = self.draw_pile[-1]
+        self.round_num = 0
+        self.round_over = False
+        self.game_over = False
+        self.current_player = 0
+
+    def get_state_of_play(self):
+        return {"round_num": self.round_num,
+                "players": self.players,
+                "current_player": self.current_player,
+                "discard_pile": self.discard_pile,
+                "draw_pile": self.draw_pile,
+                "round_over": self.round_over,
+                "game_over": self.game_over}
 
     def RequestStateOfPlay(self, request, context):
         print(f"State of Play requested by {request.name}")
-        return uno_pb2.StateOfPlay(
-            round_num = 3,
-            players = [uno_pb2.Player(
-                    hand = Deck().cards[:6],
-                    name="Test",
-                    uno_declared = False,
-                    score = 65),
-                uno_pb2.Player(
-                    hand = Deck().cards[7:9], # has to be a list of cards
-                    name="Test 2",
-                    uno_declared = False,
-                    score = 72)],
-            current_player = 1,
-            discard_pile = Deck().cards[8:53],
-            draw_pile = Deck().cards[54:89],
-            round_over = False,
-            game_over = False)
+        return uno_pb2.StateOfPlay(**self.get_state_of_play())
     
     def PlayCard(self, request, context):
         print(f"{request} played.")
@@ -71,12 +69,13 @@ class UnoServicer(uno_pb2_grpc.UnoServicer):
         if len(self.players) <= 10:
             hand = []
             for i in range(7):
-                hand.append(self.deck.pop(i))
-            return uno_pb2.Player(hand=hand,
-                                    name=request.name,
-                                    uno_declared=False,
-                                    score=0)
-
+                hand.append(self.draw_pile.pop(i))
+            new_player = uno_pb2.Player(hand=hand,
+                                        name=request.name,
+                                        uno_declared=False,
+                                        score=0)
+            self.players.append(new_player)
+            return new_player
         else:
             return uno_pb2.Player(hand=[], name="TOO MANY PLAYERS", uno_declared=True, score=-1)
 
